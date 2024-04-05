4443103852f26440336c80100f194a63c84af550
hans_wen <hans_wen@163.com>
2024-04-05 19:02:51 +0800

    添加一部分分表操作

---
+++ Admin.NET/Admin.NET.Core/SqlSugar/ISqlSugarRepository.cs
@@ -0,0 +1,79 @@
+﻿// 大名科技（天津）有限公司 版权所有
+//
+// 此源代码遵循位于源代码树根目录中的 LICENSE 文件的许可证
+//
+// 不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动
+//
+// 任何基于本项目二次开发而产生的一切法律纠纷和责任，均与作者无关
+
+namespace Admin.NET.Core;
+
+public interface ISqlSugarRepository<T> : ISugarRepository, ISimpleClient<T> where T : class, new()
+{
+    /// <summary>
+    /// 分表批量创建数据
+    /// </summary>
+    /// <param name="input">数据</param>
+    /// <returns></returns>
+    Task<bool> SplitTableInsertAsync(List<T> input);
+    /// <summary>
+    /// 分表创建数据
+    /// </summary>
+    /// <param name="input">数据</param>
+    /// <returns></returns>
+    Task<bool> SplitTableInsertAsync(T input);
+    /// <summary>
+    /// 分表批量删除数据
+    /// </summary>
+    /// <param name="input">数据</param>
+    /// <returns></returns>
+    Task<bool> SplitTableDeleteableAsync(List<T> input);
+    /// <summary>
+    /// 分表删除数据
+    /// </summary>
+    /// <param name="input">数据</param>
+    /// <returns></returns>
+    Task<bool> SplitTableDeleteableAsync(T input);
+    /// <summary>
+    /// 分表批量更新数据
+    /// </summary>
+    /// <param name="input">数据</param>
+    /// <returns></returns>
+    Task<bool> SplitTableUpdateAsync(List<T> input);
+    /// <summary>
+    /// 分表更新数据
+    /// </summary>
+    /// <param name="input">数据</param>
+    /// <returns></returns>
+    Task<bool> SplitTableUpdateAsync(T input);
+    /// <summary>
+    /// 分表判断是否存在
+    /// </summary>
+    /// <param name="whereExpression"></param>
+    /// <returns></returns>
+    Task<bool> SplitTableIsAnyAsync(Expression<Func<T, bool>> whereExpression);
+    /// <summary>
+    /// 分表获取列表
+    /// </summary>
+    /// <param name="whereExpression">条件</param>
+    /// <returns></returns>
+    Task<List<T>> SplitTableGetListAsync(Expression<Func<T, bool>> whereExpression);
+    /// <summary>
+    /// 分表获取列表
+    /// </summary>
+    /// <returns></returns>
+    Task<List<T>> SplitTableGetListAsync();
+    /// <summary>
+    /// 分表GetFirstAsyn
+    /// </summary>
+    /// <param name="whereExpression"></param>
+    /// <returns></returns>
+    Task<T> SplitTableGetFirstAsync(Expression<Func<T, bool>> whereExpression);
+    /// <summary>
+    /// 分表获取列表
+    /// </summary>
+    /// <param name="whereExpression">条件</param>
+    /// <param name="tableNames">分表表名</param>
+    /// <returns></returns>
+    Task<List<T>> SplitTableGetListAsync(Expression<Func<T, bool>> whereExpression, string[] tableNames);
+}
--- Admin.NET/Admin.NET.Core/SqlSugar/SqlSugarRepository.cs
+++ Admin.NET/Admin.NET.Core/SqlSugar/SqlSugarRepository.cs
@@ -12,7 +12,7 @@ namespace Admin.NET.Core;
 /// SqlSugar 实体仓储
 /// </summary>
 /// <typeparam name="T"></typeparam>
-public class SqlSugarRepository<T> : SimpleClient<T> where T : class, new()
+public class SqlSugarRepository<T> : SimpleClient<T>, ISqlSugarRepository<T> where T : class, new()
 {
     public SqlSugarRepository()
     {
@@ -47,4 +47,50 @@ public class SqlSugarRepository<T> : SimpleClient<T> where T : class, new()
         if (sqlSugarScopeProviderTenant == null) return;
         base.Context = sqlSugarScopeProviderTenant;
     }
+    #region 分表操作
+    public async Task<bool> SplitTableInsertAsync(T input)
+    {
+        return await base.AsInsertable(input).SplitTable().ExecuteCommandAsync() > 0;
+    }
+    public async Task<bool> SplitTableInsertAsync(List<T> input)
+    {
+        return await base.AsInsertable(input).SplitTable().ExecuteCommandAsync() > 0;
+    }
+    public async Task<bool> SplitTableUpdateAsync(T input)
+    {
+        return await base.AsUpdateable(input).SplitTable().ExecuteCommandAsync() > 0;
+    }
+    public async Task<bool> SplitTableUpdateAsync(List<T> input)
+    {
+        return await base.AsUpdateable(input).SplitTable().ExecuteCommandAsync() > 0;
+    }
+    public async Task<bool> SplitTableDeleteableAsync(T input)
+    {
+        return await base.Context.Deleteable(input).SplitTable().ExecuteCommandAsync() > 0;
+    }
+    public async Task<bool> SplitTableDeleteableAsync(List<T> input)
+    {
+        return await base.Context.Deleteable(input).SplitTable().ExecuteCommandAsync() > 0;
+    }
+    public Task<T> SplitTableGetFirstAsync(Expression<Func<T, bool>> whereExpression)
+    {
+        return base.AsQueryable().SplitTable().FirstAsync(whereExpression);
+    }
+    public Task<bool> SplitTableIsAnyAsync(Expression<Func<T, bool>> whereExpression)
+    {
+        return base.Context.Queryable<T>().Where(whereExpression).SplitTable().AnyAsync();
+    }
+    public Task<List<T>> SplitTableGetListAsync()
+    {
+        return Context.Queryable<T>().SplitTable().ToListAsync();
+    }
+    public Task<List<T>> SplitTableGetListAsync(Expression<Func<T, bool>> whereExpression)
+    {
+        return Context.Queryable<T>().Where(whereExpression).SplitTable().ToListAsync();
+    }
+    public Task<List<T>> SplitTableGetListAsync(Expression<Func<T, bool>> whereExpression, string[] tableNames)
+    {
+        return Context.Queryable<T>().Where(whereExpression).SplitTable(t => t.InTableNames(tableNames)).ToListAsync();
+    }
+    #endregion
 }
