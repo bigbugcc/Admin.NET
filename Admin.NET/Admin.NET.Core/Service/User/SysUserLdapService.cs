ef87258b675b853326e0a1b022b38257c1258578
zuohuaijun <zuohuaijun@163.com>
2024-04-13 22:44:04 +0800

    😎修复异步方法  漏了await

--- Admin.NET/Admin.NET.Core/Service/User/SysUserLdapService.cs
+++ Admin.NET/Admin.NET.Core/Service/User/SysUserLdapService.cs
@@ -48,7 +48,7 @@ public class SysUserLdapService : ITransient
     /// <returns></returns>
     public async Task AddUserLdap(long tenantId, long userId, string account, string domainAccount)
     {
-        var userLdap = _sysUserLdapRep.GetFirstAsync(u => u.TenantId == tenantId && u.IsDelete == false && (u.Account == account || u.UserId == userId || u.EmployeeId == domainAccount));
+        var userLdap = await _sysUserLdapRep.GetFirstAsync(u => u.TenantId == tenantId && u.IsDelete == false && (u.Account == account || u.UserId == userId || u.EmployeeId == domainAccount));
         if (userLdap != null)
             await _sysUserLdapRep.DeleteByIdAsync(userLdap.Id);
 
