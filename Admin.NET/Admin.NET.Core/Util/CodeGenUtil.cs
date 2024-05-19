199cf938a64a449377373dd11f27f723197a7bc2
KaneLeung <kk.xiaolan@gmail.com>
2024-05-19 02:39:37 +0000

    😦修正枚举类型在代码生成时是int而不是long
    Signed-off-by: KaneLeung <kk.xiaolan@gmail.com>

--- Admin.NET/Admin.NET.Core/Util/CodeGenUtil.cs
+++ Admin.NET/Admin.NET.Core/Util/CodeGenUtil.cs
@@ -1,4 +1,4 @@
-﻿// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
 //
 // 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 和 LICENSE-APACHE 文件。
 //
@@ -234,11 +234,11 @@ public static class CodeGenUtil
         return dataType.ToLower() switch
         {
             "tinytext" or "mediumtext" or "longtext" or "mid" or "text" or "varchar" or "char" or "nvarchar" or "nchar" or "timestamp" => "string",
-            "int" => "int",
+            "int" or "integer" => "int",
             "smallint" => "Int16",
             //"tinyint" => "byte",
             "tinyint" => "bool",    // MYSQL
-            "bigint" or "integer" => "long",
+            "bigint" => "long",
             "bit" => "bool",
             "money" or "smallmoney" or "numeric" or "decimal" => "decimal",
             "real" => "Single",
