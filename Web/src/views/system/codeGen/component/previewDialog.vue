fcc155d593d83d93a76f06d7b0e593e86bac7492
KaneLeung <kk.xiaolan@gmail.com>
2024-06-08 19:08:14 +0800

    💡移除多余的引用

--- Web/src/views/system/codeGen/component/previewDialog.vue
+++ Web/src/views/system/codeGen/component/previewDialog.vue
@@ -31,10 +31,6 @@
 <script lang="ts" setup name="sysPreviewCode">
 import { reactive, ref, nextTick, toRaw } from 'vue';
 import * as monaco from 'monaco-editor';
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 import commonFunction from '/@/utils/commonFunction';
 
