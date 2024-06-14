0c1b76b89ea9fa410dfe4b6a3e4937a0323fafe3
苏智明 <390620652@qq.com>
2024-06-14 02:40:50 +0000

    update Admin.NET/Admin.NET.Core/Job/EnumToDictJob.cs. 修复枚举到字典job没有更新枚举描述的问题
    Signed-off-by: 苏智明 <390620652@qq.com>

--- Admin.NET/Admin.NET.Core/Job/EnumToDictJob.cs
+++ Admin.NET/Admin.NET.Core/Job/EnumToDictJob.cs
@@ -1,4 +1,4 @@
-﻿// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
 //
 // 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 和 LICENSE-APACHE 文件。
 //
@@ -59,6 +59,7 @@ public class EnumToDictJob : IJob
                                 dictData.Value = enumData.Value.ToString();
                                 dictData.Code = enumData.Name;
                                 dictData.OrderNo = enumData.Value + 10;
+                                dictData.Name = enumData.Describe;
                                 uSysDictData.Add(dictData);
                             }
                         });
