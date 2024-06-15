212c63c5e894f746e890157fa11b2a737fa60fc3
npc6 <jjwsoft@qq.com>
2024-06-15 02:50:50 +0000

    update Web/src/views/system/org/index.vue. 修复编辑框机构列表树的数据不同步
    Signed-off-by: npc6 <jjwsoft@qq.com>

--- Web/src/views/system/org/index.vue
+++ Web/src/views/system/org/index.vue
@@ -107,10 +107,13 @@ const handleQuery = async (updateTree: boolean = false) => {
 	// 是否更新左侧机构列表树
 	if (updateTree == true) {
 		orgTreeRef.value?.initTreeData();
+		// 更新编辑页面机构列表树
+		var res = await getAPI(SysOrgApi).apiSysOrgListGet(0);
+		state.orgTreeData = res.data.result ?? [];
 	}
 
 	// 若无选择节点并且查询条件为空时，更新编辑页面机构列表树
-	if (state.queryParams.id == 0 && state.queryParams.name == undefined && state.queryParams.code == undefined && state.queryParams.type == undefined) state.orgTreeData = state.orgData;
+	if (state.queryParams.id == 0 && state.queryParams.name == undefined && state.queryParams.code == undefined && state.queryParams.type == undefined && updateTree == false) state.orgTreeData = state.orgData;
 };
 
 // 重置操作
