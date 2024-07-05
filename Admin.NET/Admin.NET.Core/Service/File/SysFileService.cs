7f1e2b9cbeb6997f024fb6993c5463adf39b092c
IMaster <logole@live.cn>
2024-07-05 10:24:20 +0000

    修复上传头像提示D8003
    Signed-off-by: IMaster <logole@live.cn>

--- Admin.NET/Admin.NET.Core/Service/File/SysFileService.cs
+++ Admin.NET/Admin.NET.Core/Service/File/SysFileService.cs
@@ -1,4 +1,4 @@
-﻿// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
 //
 // 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 和 LICENSE-APACHE 文件。
 //
@@ -334,6 +334,11 @@ public class SysFileService : IDynamicApiController, ITransient
             if (suffix == ".jpeg" || suffix == ".jpe")
                 suffix = ".jpg";
         }
+        
+        // 获取后缀名失败 并且 上传文件为 blob
+        if (suffix == "" && file.FileName == "blob")
+            suffix = file.ContentType.Substring(file.ContentType.LastIndexOf('/') + 1); // file.ContentType.Split('/')[1]
+
         if (string.IsNullOrWhiteSpace(suffix))
             throw Oops.Oh(ErrorCodeEnum.D8003);
 
