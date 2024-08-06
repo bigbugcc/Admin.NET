a96998b254ff4439b3b7c2773421321f69dc20ce
490912587@qq.com <490912587@qq.com>
2024-08-06 11:36:18 +0800

    fastcrud集成

--- Web/src/main.ts
+++ Web/src/main.ts
@@ -24,5 +24,86 @@ const app = createApp(App);
 
 directive(app);
 other.elSvg(app);
-
+//#region  FastCrud配置
+import { FastCrud } from "@fast-crud/fast-crud";
+import "@fast-crud/fast-crud/dist/style.css";
+import ui from "@fast-crud/ui-element";
+import { FsExtendsUploader, FsExtendsEditor } from "@fast-crud/fast-extends";
+import "@fast-crud/fast-extends/dist/style.css";
+import { getToken } from '/@/utils/axios-utils';
+app.use(ui);
+app.use(FastCrud, {
+    i18n,
+    commonOptions() {
+        return {
+            request: {
+                transformQuery: ({ page, form, sort }) => {
+                    const order = sort == null ? {} : { orderProp: sort.prop, orderAsc: sort.asc }
+                    return { page: page.currentPage, pageSize: page.pageSize, ...form, ...order };
+                },
+                // page请求结果转换
+                transformRes: ({ res }) => {
+                    const records = res.data.result.items;
+                    const total = res.data.result.total;
+                    const currentPage = res.data.result.page;
+                    const pageSize = res.data.result.pageSize;
+                    return {
+                        currentPage: currentPage, pageSize: pageSize, total: total, records
+                    }
+                },
+                form: {
+                    display: 'flex', // 表单布局
+                    labelWidth: '120px' // 表单label宽度
+                }
+            },
+        };
+    },
+});
+const baseURL = import.meta.env.VITE_API_URL;
+import request from '/@/utils/request';
+//文件上传
+app.use(FsExtendsUploader, {
+    defaultType: 'form',
+    form: {
+        action: baseURL + '/api/sysFile/uploadFile',
+        name: 'file',
+        withCredentials: false,
+        uploadRequest: async props => {
+            const { action, file, onProgress } = props;
+            const data = new FormData();
+            data.append('file', file);
+            const token = getToken();
+            const Authorization = token ? `Bearer ${token}` : null;
+            const result = await request({
+                url: action,
+                method: 'post',
+                data,
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    "Authorization": Authorization
+                },
+                timeout: 60000,
+                onUploadProgress(progress) {
+                    onProgress({ percent: Math.round((progress.loaded / progress.total!) * 100) });
+                }
+            });
+            if (result) {
+                return result.data
+            } else {
+                throw new Error(result.message);
+            }
+        },
+        async successHandle(ret: any) {
+            return {
+                url: baseURL + "/" + ret.result.filePath + "/" + ret.result.id + ret.result.suffix,
+                key: ret.result.fileName
+            };
+        }
+    }
+});
+//富文本编辑器
+app.use(FsExtendsEditor, {
+    wangEditor: {}
+});
+//#endregion
 app.use(pinia).use(router).use(ElementPlus).use(i18n).use(VueGridLayout).use(VForm3).use(VueSignaturePad).use(vue3TreeOrg).mount('#app');
