db973371fb242d6c8222d11547b8e213f3622383
X5ZJ <x5zj@qq.com>
2024-08-10 06:09:10 +0000

    update Web/src/views/home/widgets/components/schedule.vue. 优化 我的日程。“添加日历”按钮时默认是今日（而不是随着点击日历中的那一天同步），修改后，选定日历中的哪一天 添加日程的就是对应的那一天（时间段不变）。可能更为合理！
    Signed-off-by: X5ZJ <x5zj@qq.com>

--- Web/src/views/home/widgets/components/schedule.vue
+++ Web/src/views/home/widgets/components/schedule.vue
@@ -77,6 +77,7 @@ const state = reactive({
 	TodayScheduleData: [] as Array<SysSchedule>, // 当天列表数据
 	calendarValue: new Date(),
 	queryParams: {
+        scheduleTime: new Date(),
 		startTime: new Date(),
 		endTime: new Date(),
 	},
@@ -159,7 +160,7 @@ const openAddSchedule = () => {
 	var timerange = GetRecentTime();
 
 	state.editTitle = '添加日程';
-	editScheduleRef.value?.openDialog({ id: undefined, status: 0, orderNo: 100, startTime: timerange.startTime, endTime: timerange.endTime });
+	editScheduleRef.value?.openDialog({ id: undefined, status: 0, orderNo: 100, scheduleTime: state.queryParams.scheduleTime, startTime: timerange.startTime, endTime: timerange.endTime });
 };
 
 // 打开编辑页面
@@ -175,6 +176,7 @@ const handleClickDate = async (data: any) => {
 		await handleQuery();
 	}
 	await handleQueryByDate(data.day);
+    state.queryParams.scheduleTime=data.day;
 };
 
 // 获取当月第一天
