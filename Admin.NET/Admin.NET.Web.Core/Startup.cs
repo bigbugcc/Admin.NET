8ccfddcce4128a99ce0e789d97a853af5e574824
Lzh666 <422235757@qq.com>
2024-10-12 13:55:54 +0000

    update Admin.NET/Admin.NET.Web.Core/Startup.cs. 在Application层中注入应用服务时出现异常，原因是调用了Core层的部分服务且Core层未执行Startup；将Core的AppStartup配置为先于App层执行
    Signed-off-by: Lzh666 <422235757@qq.com>

--- Admin.NET/Admin.NET.Web.Core/Startup.cs
+++ Admin.NET/Admin.NET.Web.Core/Startup.cs
@@ -1,4 +1,4 @@
-﻿// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
 //
 // 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 和 LICENSE-APACHE 文件。
 //
@@ -30,6 +30,7 @@ using System.Threading.Tasks;
 
 namespace Admin.NET.Web.Core;
 
+[AppStartup(int.MaxValue)]//将Order置最大值确保最先执行
 public class Startup : AppStartup
 {
     public void ConfigureServices(IServiceCollection services)
