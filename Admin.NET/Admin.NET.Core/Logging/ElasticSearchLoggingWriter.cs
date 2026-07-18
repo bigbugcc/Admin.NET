456c54b92b3cb77433101673254479ad0980f729
蓝猴子 <czieboy@live.cn>
2026-07-18 11:06:00 +0000

    update Admin.NET/Admin.NET.Core/Logging/ElasticSearchLoggingWriter.cs. 同上一个PR
    Signed-off-by: 蓝猴子 <czieboy@live.cn>

--- Admin.NET/Admin.NET.Core/Logging/ElasticSearchLoggingWriter.cs
+++ Admin.NET/Admin.NET.Core/Logging/ElasticSearchLoggingWriter.cs
@@ -1,4 +1,4 @@
-﻿// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
+// Admin.NET 项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
 //
 // 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE-MIT 和 LICENSE-APACHE 文件。
 //
@@ -94,7 +94,7 @@ public class ElasticSearchLoggingWriter : IDatabaseLoggingWriter, IDisposable
             ControllerName = loggingMonitor.ControllerName,                          // 控制器名称
             ActionName = loggingMonitor.ActionTypeName,                              // 操作方法名称
             DisplayTitle = loggingMonitor.DisplayTitle,                              // 显示标题
-            Status = loggingMonitor.ReturnInformation?.HttpStatusCode.ToString(),    // HTTP状态码
+            Status = loggingMonitor.ReturnInformation?.HttpStatusCode.ToString() ?? "200",    // HTTP状态码
             RemoteIp = remoteIPv4,                                                   // 客户端IP
             Location = ipLocation,                                                   // IP地理位置
             Longitude = (decimal)longitude,                                                   // 经度
